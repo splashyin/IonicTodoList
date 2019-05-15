49feb15a305ce1b75b0276f3fd843ceef1ce7c6b
Anthony Yin <anthony.yin@fultonhogan.com>
2019-05-15 12:40:30 +1200

    add cloud storage CRUD function

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {ModalController, Events} from '@ionic/angular';
+import { ModalController, Events } from '@ionic/angular';
 import { ToDoItem, ToDoList } from './../../classes/item.class';
+import { AmplifyService } from 'aws-amplify-angular';
 import { ListItemModal } from './list.item.modal';
 @Component({
 
@@ -9,33 +10,39 @@ import { ListItemModal } from './list.item.modal';
   styleUrls: ['./list.page.scss'],
 })
 export class ListPage implements OnInit {
-
+  amplifyService: AmplifyService;
   modal: any;
   data: any;
   user: any;
-  itemList: ToDoList;
+  itemList: ToDoList | any;
   signedIn: boolean;
 
   constructor(
     public modalController: ModalController,
+    amplify: AmplifyService,
     events: Events
   ) {
+    this.amplifyService = amplify;
     events.subscribe('data:AuthState', async (data) => {
       if (data.loggedIn) {
+        this.user = await this.amplifyService.auth().currentUserInfo();
         this.getItems();
       } else {
-        // this.itemList.items = [];
+        this.itemList.items = [];
+        this.user = null;
       }
     });
   }
 
   async ngOnInit() {
+    // use amplify to get user data 
+    this.user = await this.amplifyService.auth().currentUserInfo();
     this.getItems();
   }
 
   async modify(item, i) {
     let props = {
-      itemList: this.itemList,
+      itemList: this.itemList || new ToDoList({ userId: this.user.id }),
       editItem: item || undefined
     };
 
@@ -44,7 +51,7 @@ export class ListPage implements OnInit {
       componentProps: props
     });
 
-    this.modal.onDidDismiss( (result) => {
+    this.modal.onDidDismiss().then((result) => {
       if (result.data.newItem) {
         result.data.itemList.items.push(result.data.newItem);
       } else if (result.data.editItem) {
@@ -57,34 +64,42 @@ export class ListPage implements OnInit {
 
   delete(i) {
     this.itemList.items.splice(i, 1);
+    this.save(this.itemList);
+
   }
 
   complete(i) {
     this.itemList.items[i].status = 'complete';
+    this.save(this.itemList);
+
   }
 
-  save(list){
+  save(list) {
     // Use AWS Amplify to save the list...
-    // this.itemList = list;
+    this.amplifyService.api().post('yinhaoToDoItemsCRUDtest', '/ToDoItems', { body: list }).then((i) => {
+      this.getItems();
+    })
+      .catch((err) => {
+        console.log('Error saving list: ${err}');
+      });
   }
 
   getItems() {
-    this.itemList = {
-      userId: 1,
-      items: [
-        new ToDoItem({
-          id: '1',
-          title: 'test item 1',
-          description: 'my test item',
-          status: 'complete'
-        }),
-        new ToDoItem({
-          id: '2',
-          title: 'test item 3',
-          description: 'my other test item',
-          status: 'pending'
-        })
-      ]
+    if (this.user) {
+      this.amplifyService.api().get('yinhaoToDoItemsCRUDtest', `/ToDoItems/${this.user.id}`, {}).then((res) => {
+        if (res && res.length > 0) {
+          this.itemList = res[0];
+        } else {
+          this.itemList = new ToDoList({ userId: this.user.id, items: [] });
+        }
+      })
+        .catch((err) => {
+          console.log('Error getting list: ${err}');
+        });
+    } else {
+      console.log('Cannot get items: no active user');
     }
+
   }
+
 }
